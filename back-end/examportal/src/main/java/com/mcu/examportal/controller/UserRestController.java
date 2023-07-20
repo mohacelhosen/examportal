094199b960ba6732c39e14ae4d200f1998366b43
mohacelhosen <mohacel.hosen22@gmail.com>
2023-07-20 14:43:00 +0600

    Controller method added

--- back-end/examportal/src/main/java/com/mcu/examportal/controller/UserRestController.java
+++ back-end/examportal/src/main/java/com/mcu/examportal/controller/UserRestController.java
@@ -22,9 +22,24 @@ public class UserRestController {
     }
 
 //    <-------------------- Find user by Email ------------------>
-    @GetMapping("/{userEmail}")
+    @GetMapping("/find/{userEmail}")
     public  ResponseEntity<UserModel> findUserByEmail(@PathVariable String userEmail){
         UserModel user = userService.getUser(userEmail);
         return new ResponseEntity<>(user, HttpStatus.FOUND);
     }
+
+//    <-------------------- update user information ------------------>
+    @PutMapping("/update")
+    public  ResponseEntity<UserModel> updateUserInfo(@RequestBody UserModel user){
+        UserModel updateUserInfo = userService.updateUserInfo(user);
+        return new ResponseEntity<>(updateUserInfo, HttpStatus.OK);
+    }
+
+//    <-------------------- update user information ------------------>
+    @DeleteMapping("/delete/{userEmail}")
+    public ResponseEntity<String> deleteUser(@PathVariable String userEmail){
+        String message = userService.deleteUser(userEmail);
+        return new ResponseEntity<>(message, HttpStatus.OK);
+    }
+
 }
